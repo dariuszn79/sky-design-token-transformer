724f730b0de60f1cb776a4e46a4f3c2dedd4d406
dariuszn79 <dariuszn79@users.noreply.github.com>
2023-10-12 09:57:22 +0000

    Tokens updated

--- build/ios-swift/StyleDictionarySize.swift
+++ build/ios-swift/StyleDictionarySize.swift
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Fri, 15 Jul 2022 19:49:49 GMT
+// Generated on Thu, 12 Oct 2023 09:57:22 GMT
 
 
 import UIKit
@@ -16,4 +16,20 @@ public enum StyleDictionarySize {
     public static let bodyItalicFontWeight = 400
     public static let bodyMediumExtendedItalicFontWeight = 500
     public static let bodySuperFontWeight = 900
+    public static let medBreadcrumbsLrgFontWeight = 400
+    public static let medBreadcrumbsSmlFontWeight = 500
+    public static let medButtonLrgFontWeight = 500
+    public static let medButtonSmlFontWeight = 500
+    public static let medCaptionFontWeight = 400
+    public static let medFootnoteFontWeight = 400
+    public static let medH1FontWeight = 300
+    public static let medH2FontWeight = 400
+    public static let medH3FontWeight = 400
+    public static let medH4FontWeight = 500
+    public static let medH5FontWeight = 500
+    public static let medH6FontWeight = 500
+    public static let medLabelBoldFontWeight = 500
+    public static let medLabelFontWeight = 400
+    public static let medLinkFontWeight = 400
+    public static let medPFontWeight = 400
 }
--- build/ios/StyleDictionaryColor.h
+++ build/ios/StyleDictionaryColor.h
@@ -4,12 +4,47 @@
 //
 
 // Do not edit directly
-// Generated on Fri, 15 Jul 2022 19:49:49 GMT
+// Generated on Thu, 12 Oct 2023 09:57:22 GMT
 
 
 #import <UIKit/UIKit.h>
 
 typedef NS_ENUM(NSInteger, StyleDictionaryColorName) {
+ColorPrimaryPrimary100,
+ColorPrimaryPrimary75,
+ColorPrimaryPrimary50,
+ColorPrimaryPrimary25,
+ColorPrimaryPrimary05,
+ColorSecondarySecondary100,
+ColorSecondarySecondary50,
+ColorSecondarySecondary75,
+ColorSecondarySecondary25,
+ColorSecondarySecondary05,
+ColorTertiaryTertiary100,
+ColorTertiaryTertiary75,
+ColorTertiaryTertiary50,
+ColorTertiaryTertiary25,
+ColorTertiaryTertiary05,
+ColorLightLight100,
+ColorLightLight75,
+ColorLightLight50,
+ColorLightLight25,
+ColorLightLight05,
+ColorLightWhite,
+ColorLightTransparent,
+ColorDarkBlack100,
+ColorDarkBlack75,
+ColorDarkBlack50,
+ColorDarkBlack25,
+ColorDarkBlack05,
+ColorFunctionalError,
+ColorFunctionalWarning,
+ColorFunctionalSucess,
+ColorMaterialsG1Blue,
+ColorMaterialsG1Green,
+ColorMaterialsG1Pink,
+ColorMaterialsG1Black,
+ColorMaterialsG1White,
 ColorColorsSemiRed,
 ColorColorsMultipleFills0,
 ColorColorsMultipleFills1,

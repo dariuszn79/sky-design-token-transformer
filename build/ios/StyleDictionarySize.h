d7dc69b856a613fa453a714c96de820ffe07c502
dariuszn79 <dariuszn79@users.noreply.github.com>
2023-10-12 09:58:46 +0000

    Tokens updated

--- build/ios/StyleDictionarySize.h
+++ build/ios/StyleDictionarySize.h
@@ -3,12 +3,28 @@
 //
 
 // Do not edit directly
-// Generated on Fri, 15 Jul 2022 19:49:49 GMT
+// Generated on Thu, 12 Oct 2023 09:58:46 GMT
 
 
 #import <Foundation/Foundation.h>
 
 
+extern float const TypographyMedH1FontWeight;
+extern float const TypographyMedH2FontWeight;
+extern float const TypographyMedH3FontWeight;
+extern float const TypographyMedH4FontWeight;
+extern float const TypographyMedH5FontWeight;
+extern float const TypographyMedH6FontWeight;
+extern float const TypographyMedPFontWeight;
+extern float const TypographyMedCaptionFontWeight;
+extern float const TypographyMedButtonLrgFontWeight;
+extern float const TypographyMedButtonSmlFontWeight;
+extern float const TypographyMedLabelFontWeight;
+extern float const TypographyMedLabelBoldFontWeight;
+extern float const TypographyMedLinkFontWeight;
+extern float const TypographyMedBreadcrumbsLrgFontWeight;
+extern float const TypographyMedBreadcrumbsSmlFontWeight;
+extern float const TypographyMedFootnoteFontWeight;
 extern float const TypographyBodyH3FontWeight;
 extern float const TypographyBodyH4StrikeThroughFontWeight;
 extern float const TypographyBodyItalicFontWeight;
--- build/ios/StyleDictionarySize.m
+++ build/ios/StyleDictionarySize.m
@@ -4,12 +4,28 @@
 //
 
 // Do not edit directly
-// Generated on Fri, 15 Jul 2022 19:49:49 GMT
+// Generated on Thu, 12 Oct 2023 09:58:46 GMT
 
 
 #import "StyleDictionarySize.h"
 
 
+float const TypographyMedH1FontWeight = 300;
+float const TypographyMedH2FontWeight = 400;
+float const TypographyMedH3FontWeight = 400;
+float const TypographyMedH4FontWeight = 500;
+float const TypographyMedH5FontWeight = 500;
+float const TypographyMedH6FontWeight = 500;
+float const TypographyMedPFontWeight = 400;
+float const TypographyMedCaptionFontWeight = 400;
+float const TypographyMedButtonLrgFontWeight = 500;
+float const TypographyMedButtonSmlFontWeight = 500;
+float const TypographyMedLabelFontWeight = 400;
+float const TypographyMedLabelBoldFontWeight = 500;
+float const TypographyMedLinkFontWeight = 400;
+float const TypographyMedBreadcrumbsLrgFontWeight = 400;
+float const TypographyMedBreadcrumbsSmlFontWeight = 500;
+float const TypographyMedFootnoteFontWeight = 400;
 float const TypographyBodyH3FontWeight = 700;
 float const TypographyBodyH4StrikeThroughFontWeight = 500;
 float const TypographyBodyItalicFontWeight = 400;
